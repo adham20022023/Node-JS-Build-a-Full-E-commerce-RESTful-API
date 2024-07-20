159f1dd7344a1b6596b9bd9a721bce41143a94b6
adham20022023 <adham2002020@protonmail.com>
2024-07-20 23:24:07 +0300

    .

--- config/database.js
+++ config/database.js
@@ -1,29 +1,23 @@
-const mongoose = require('mongoose');
-const dbConnection = () =>{
-    //! DB Connection
-    const DB = process.env.DATABASE.replace(
-        '<PASSWORD>',
-        process.env.DATABASE_PASSWORD
-    )
-    mongoose
-    .connection.on('disconnected',
-    () => console.log('disconnected..')
-    );
-    mongoose
-    .connection.on('reconnected',
-    () => console.log('reconnected...')
-    );
+const mongoose = require("mongoose");
+const dbConnection = () => {
+  //! DB Connection
+  const DB = process.env.DATABASE.replace(
+    "<PASSWORD>",
+    process.env.DATABASE_PASSWORD
+  );
+  mongoose.connection.on("disconnected", () => console.log("disconnected.."));
+  mongoose.connection.on("reconnected", () => console.log("reconnected..."));
 
-    mongoose
-        .connect(DB, {
-            //? we can type some options here 
-        })
-        .then((conn) => {        
-            console.log(`DB connection successful! 🥳 ${conn.connection.host}`);
-        })
-        .catch((err) => {
-            console.log("Database connection failed!", err);
-            process.exit(1);
-        })
-}
-module.exports = dbConnection
+  mongoose
+    .connect(DB, {
+      //? we can type some options here
+    })
+    .then((conn) => {
+      console.log(`DB connection successful! 🥳 ${conn.connection.host}`);
+    })
+    .catch((err) => {
+      console.log("Database connection failed!", err);
+      process.exit(1);
+    });
+};
+module.exports = dbConnection;
